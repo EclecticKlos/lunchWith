ad81541c063ae93fa10e4fb23eb320191706ae69
Dan Klos <dsklos@gmail.com>
2016-04-01 16:58:42 -0700

    Implemented without DB connection, using new lunchZip service to pass lunchZip from LinkedinCtrl to FoodiesCtrl

--- lunchWith/www/js/controllers.js
+++ lunchWith/www/js/controllers.js
@@ -1,7 +1,9 @@
 angular.module('lunchWith.controllers', [])
 
 
-.controller('LinkedinCtrl', function($scope, $cordovaOauth, $ionicModal, $cordovaKeyboard, LinkedinAPIRequest) {
+.controller('LinkedinCtrl', ['$scope',"$cordovaOauth", "$ionicModal", "$cordovaKeyboard", "LinkedinAPIRequest", "lunchZipService", function($scope, $cordovaOauth, $ionicModal, $cordovaKeyboard, LinkedinAPIRequest, lunchZipService) {
+
+  $scope.lunchZip = { LunchZip: "Zip Code"};
 
     // Zipcode Modal
   $ionicModal.fromTemplateUrl('templates/lunch-zip.html', {
@@ -37,14 +39,15 @@ angular.module('lunchWith.controllers', [])
   $scope.closeLunchZip = function () {
     // post model to api
     // $scope.lunchZip
-    console.log($scope.lunchZip);
+    console.log($scope.lunchZip.LunchZip);
+    lunchZipService.setLunchZip($scope.lunchZip.LunchZip)
     $scope.modal.hide();
     $scope.modal.remove();
   };
 
-})
+}])
 
-.controller('FoodiesCtrl', function($scope) {
+.controller('FoodiesCtrl',['$scope', 'lunchZipService', function($scope, lunchZipService) {
   var counter = 0;
   var foodieList = [
     {
@@ -55,17 +58,19 @@ angular.module('lunchWith.controllers', [])
       "url": "https://www.linkedin.com/profile/view?id=AAoAAAOxcOwBxJIttVPHY0yu11x4dFFM6-_mzMc&authType=name&authToken=YfxC&trk=api*a3227641*s3301901*",
       "numConnections": 314,
       "pictureUrl": "https://media.licdn.com/mpr/mprx/0_GoniwO0q-HTVQ1mQGd5ywgU6-INqE1mQ8DCywgyqDa8XvBM6m2QfLjHVp5qIdq7EidNjkVm14yj_",
-      "bio": "I like to eat eat eat apples and bananas I like to eat eat eat apples and bananasI like to eat eat eat apples and bananasI like to eat eat eat apples and bananas"
+      "bio": "I like to eat eat eat apples and bananas I like to eat eat eat apples and bananasI like to eat eat eat apples and bananasI like to eat eat eat apples and bananas",
+      "lunchZip": "94107"
     },
    {
       "linkedinId": "jHi2RNppDF",
-      "firstName": "Bob",
-      "lastName": "Dole",
+      "firstName": "Notmy",
+      "lastName": "LunchZip",
       "headline": "Web Apps Developer",
       "url": "https://www.linkedin.com/profile/view?id=AAoAAAOxcOwBxJIttVPHY0yu11x4dFFM6-_mzMc&authType=name&authToken=YfxC&trk=api*a3227641*s3301901*",
       "numConnections": 14,
       "pictureUrl": "https://media.licdn.com/mpr/mprx/0_GoniwO0q-HTVQ1mQGd5ywgU6-INqE1mQ8DCywgyqDa8XvBM6m2QfLjHVp5qIdq7EidNjkVm14yj_",
-      "bio": "I like to eat eat eat apples and bananas I like to eat eat eat apples and bananasI like to eat eat eat apples and bananasI like to eat eat eat apples and bananas"
+      "bio": "I like to eat eat eat apples and bananas I like to eat eat eat apples and bananasI like to eat eat eat apples and bananasI like to eat eat eat apples and bananas",
+      "lunchZip": "94103"
     },
     {
       "linkedinId": "jHi2RNppLe",
@@ -75,40 +80,48 @@ angular.module('lunchWith.controllers', [])
       "url": "https://www.linkedin.com/profile/view?id=AAoAAAOxcOwBxJIttVPHY0yu11x4dFFM6-_mzMc&authType=name&authToken=YfxC&trk=api*a3227641*s3301901*",
       "numConnections": 31,
       "pictureUrl": "https://media.licdn.com/mpr/mprx/0_GoniwO0q-HTVQ1mQGd5ywgU6-INqE1mQ8DCywgyqDa8XvBM6m2QfLjHVp5qIdq7EidNjkVm14yj_",
-      "bio": "I like to eat eat eat apples and bananas I like to eat eat eat apples and bananasI like to eat eat eat apples and bananasI like to eat eat eat apples and bananas"
+      "bio": "I like to eat eat eat apples and bananas I like to eat eat eat apples and bananasI like to eat eat eat apples and bananasI like to eat eat eat apples and bananas",
+      "lunchZip": "94107"
     }
   ];
 
   $scope.currentFoodie = foodieList[counter];
 
   $scope.nextProfile = function() {
-    counter++;
-    $scope.currentFoodie = foodieList[counter];
+    if (counter < foodieList.length){
+      counter++;
+      $scope.currentFoodie = foodieList[counter];
+      while (counter < foodieList.length && lunchZipService.getLunchZip() !== $scope.currentFoodie.lunchZip) {  // Advance to next foodie with matching lunchZip
+        counter++;
+        $scope.currentFoodie = foodieList[counter];
+      }
+    }
   };
 
-})
+}])
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   // With the new view caching in Ionic, Controllers are only called
+//   // when they are recreated or on app start, instead of every page change.
+//   // To listen for when this page is active (for example, to refresh data),
+//   // listen for the $ionicView.enter event:
+//   //
+//   //$scope.$on('$ionicView.enter', function(e) {
+//   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   };
+// })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// })
+;
