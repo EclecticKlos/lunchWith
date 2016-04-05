fd1fcdaae6c3f3e1d760736db83a0507998b7812
Thuong Ho <thuong.t.ho@gmail.com>
2016-04-04 23:27:43 -0700

    remove auto lint in gulp, add get method that returns profile when using foodieId

--- lunchWithAPI/controllers/foodieController.js
+++ lunchWithAPI/controllers/foodieController.js
@@ -3,19 +3,28 @@
 
 	var foodieController = function (Foodie) {
 		// find the profile
-		var findFoodie = function(request, response) {
+		var findFoodie = function(request, response, next) {
 			Foodie.findById(request.params.foodieId, function (error, foodie) {
 				if (error) {
 					response.status(500);
 					response.send(error);
+				} else if (foodie) {
+					request.foodie = foodie;
+					next();
 				} else {
-					response.foodie = foodie;
+					response.status(404);
+					response.send('No profile found');
 				}
 			})
 		};
 
+		var get = function (request, response) {
+			response.json(request.foodie);
+		};
+
 		return {
-			findFoodie: findFoodie
+			findFoodie: findFoodie,
+			get: get
 		};
 	};
 
